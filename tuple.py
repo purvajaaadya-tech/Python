81ddc8f5fdacccbc843fc9e00b808e3826463223
purvajaaadya-tech <purvaja.aadya@gmail.com>
2026-09-02 19:39:02 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,19 @@
+t = ("tuple", False, 3.2, 1)
+print(t)
+
+t = (4, 6, 2, 8, 3, 1)
+print(t)
+
+t = t + (9,)
+print(t)
+
+t1 = (50, 10, 60, 70, 50)
+print(t1.count(50))
+
+t = (2, 4, 3, 5, 4, 6, 7, 8, 6, 1)
+
+_slice = t[3:5]
+print(_slice)
+
+_slice = t[:6]
+print(_slice)
