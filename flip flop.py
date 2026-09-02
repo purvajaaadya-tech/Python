cf58e827fb8018209208e2cc70990649cb7e8e97
purvajaaadya-tech <purvaja.aadya@gmail.com>
2026-09-02 19:18:10 +0530

    Add files via upload

---
+++ flip flop.py
@@ -0,0 +1,16 @@
+def p(r):
+    e = len(r) -1
+    s = 0
+    while(s<e):
+        if(r[s]!=r[e]):
+            return False
+        s+=1
+        e-=1
+    return True
+
+r = (1,2,3,3,2,1)
+
+if(p(r)):
+    print("The tuple is flip-flop")
+else:
+    print("The tuple is not flip-flop")
