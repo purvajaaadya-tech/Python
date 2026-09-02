dbbd556adcc6822f0c5e18bb86f97e0de2cfbeae
purvajaaadya-tech <purvaja.aadya@gmail.com>
2026-09-02 19:31:04 +0530

    Add files via upload

---
+++ weather pred.py
@@ -0,0 +1,13 @@
+w = (1, 0, 0, 0, 1, 1, 0)
+s = 0
+r = 0
+for i in range(0,7):
+    if(w[i]==0):
+        r+=1
+    else:
+        s+=1
+
+if(s>r):
+    print("Good weather")
+else:
+    print("Bad weather")
