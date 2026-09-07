5aa74087567a4310bcc8ddf6780ca9b0fb5031ef
purvajaaadya-tech <purvaja.aadya@gmail.com>
2026-09-07 19:37:55 +0530

    Add files via upload

---
+++ country code.py
@@ -0,0 +1,9 @@
+cc =   {'India' : '0091',
+        'Australia' : '0025',
+        'Nepal' : '00977'}
+
+print("Country code for India - ")
+print(cc.get('India', 'Not Found'))
+
+print("Country code for Japan - ")
+print(cc.get('Japan', 'Not Found'))
